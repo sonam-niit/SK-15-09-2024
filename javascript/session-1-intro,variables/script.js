781c5ad1ac7b3e3f860b94be9c6e79ec018fb645
sonam-niit <sonam.gravity@gmail.com>
2024-11-30 21:55:14 +0530

    javascript intro, variables, operators

---
+++ javascript/session-1-intro,variables/script.js
@@ -0,0 +1,9 @@
+//Single Line Comment
+/*
+    Author: Sonam Soni
+    Project: To Include JS Code
+    Date: 30th Nov 2024
+*/
+alert('Hello From External JS')
+console.log('External Script')
+document.write("<h1>Hello From External</h1>")
