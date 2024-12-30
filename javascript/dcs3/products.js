3f81b189ae3a5e6927bc15a14cd2544c272bb40f
sonam-niit <sonam.gravity@gmail.com>
2024-12-30 22:04:33 +0530

    Doubt sesion 3

---
+++ javascript/dcs3/products.js
@@ -0,0 +1,34 @@
+const products=[
+    {
+        id:1,
+        name: "Essence Mascara Lash Princess",
+        description: "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
+        category: "beauty",
+        price: 9.99,
+        imgUrl: "https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/1.png"
+    },
+    {
+        id:2,
+        name: "Eyeshadow Palette with Mirror",
+        description: "The Eyeshadow Palette with Mirror offers a versatile range of eyeshadow shades for creating stunning eye looks. With a built-in mirror, it's convenient for on-the-go makeup application.",
+        category: "beauty",
+        price: 19.99,
+        imgUrl: "https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/1.png"
+    },
+    {
+        id:3,
+        name: "Powder Canister",
+        description: "The Powder Canister is a finely milled setting powder designed to set makeup and control shine. With a lightweight and translucent formula, it provides a smooth and matte finish.",
+        category: "beauty",
+        price: 14.99,
+        imgUrl: "https://cdn.dummyjson.com/products/images/beauty/Powder%20Canister/1.png"
+    },
+    {
+        id:4,
+        name: "Red Lipstick",
+        description:"The Red Lipstick is a classic and bold choice for adding a pop of color to your lips. With a creamy and pigmented formula, it provides a vibrant and long-lasting finish.",
+        category: "beauty",
+        price: 12.99,
+        imgUrl: "https://cdn.dummyjson.com/products/images/beauty/Red%20Lipstick/1.png"
+    }
+]
