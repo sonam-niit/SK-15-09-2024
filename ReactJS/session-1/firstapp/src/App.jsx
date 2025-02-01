952c97f80a186e6b82cf1e530aa39c1489b743ed
sonam-niit <sonam.gravity@gmail.com>
2025-02-01 21:52:11 +0530

    React Intro

---
+++ ReactJS/session-1/firstapp/src/App.jsx
@@ -0,0 +1,11 @@
+import Greeting from "./Greeting"
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the world of React</h1>
+      <Greeting />
+    </div>
+  )
+}
+export default App
---
+++ ReactJS/session-1/firstapp/src/Greeting.jsx
@@ -0,0 +1,9 @@
+function Greeting(){
+
+    return(
+        <div>
+            <h2>good Morning from Greeting Component</h2>
+        </div>
+    )
+}
+export default Greeting;
